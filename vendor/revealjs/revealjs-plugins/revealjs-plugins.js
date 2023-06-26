0b0db9b6c36bd0b8fb8fa6e21284126e918ae47e
Torsten Kleiber <torsten.kleiber@web.de>
2023-06-26 22:44:55 +0200

    Revert "update reveal.js plugin chalkboard"
    This reverts commit 40d0d74bec1a2491ebc1e4c261d52af691e9225e.

--- vendor/revealjs/revealjs-plugins/revealjs-plugins.js
+++ vendor/revealjs/revealjs-plugins/revealjs-plugins.js
@@ -1,5 +1,5 @@
 { src: 'assets/vendor/revealjs/revealjs-plugins/menu/menu.js', async: true },
-{ src: 'assets/vendor/revealjs/revealjs-plugins/chalkboard/plugin.js', async: true },
+{ src: 'assets/vendor/revealjs/revealjs-plugins/chalkboard/chalkboard.js', async: true },
 { src: 'assets/vendor/revealjs/revealjs-plugins/title-footer/title-footer.js', callback: function() { title_footer.initialize(); } },//not async because it may then not appear
 { src: 'assets/vendor/revealjs/revealjs-plugins/notes-pointer/notes-pointer.js', async: true },
 { src: 'assets/vendor/revealjs/revealjs-plugins/spotlight/spotlight.js' }, // does not work with current version of reveal.js
