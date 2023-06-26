2d482c39132bcb55e4fb8e9ab51b31c7a1365bf0
Torsten Kleiber <torsten.kleiber@web.de>
2023-06-26 22:40:47 +0200

    Revert "update reveal.js plugin menu"
    This reverts commit ee1f53eefc05b6b83385cf2b0cf10145b7a84707.

--- vendor/revealjs/revealjs-plugins/revealjs-plugins.js
+++ vendor/revealjs/revealjs-plugins/revealjs-plugins.js
@@ -1,4 +1,4 @@
-{ src: 'assets/vendor/revealjs/revealjs-plugins/menu/plugin.js', async: true },
+{ src: 'assets/vendor/revealjs/revealjs-plugins/menu/menu.js', async: true },
 { src: 'assets/vendor/revealjs/revealjs-plugins/chalkboard/plugin.js', async: true },
 { src: 'assets/vendor/revealjs/revealjs-plugins/title-footer/title-footer.js', callback: function() { title_footer.initialize(); } },//not async because it may then not appear
 { src: 'assets/vendor/revealjs/revealjs-plugins/notes-pointer/notes-pointer.js', async: true },
